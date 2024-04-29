ebd63d396c2384c4b5d1ab6463f2f08833101d6a
Georges Monette <georges+cv@yorku.ca>
2024-04-29 18:00:02 +0000

    version 2.0.0

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 
 <!-- badges: end -->
 
-# cv package for R: Cross-Validation of Regression Models
+# cv package for R: Cross-Validating Regression Models
 
 <img src="man/figures/cv-hex.png" style="float:right; height:200px;"/>
 
@@ -16,15 +16,17 @@ The **cv** package for R provides a consistent and extensible framework for cros
 
 -   `mse()` (mean-squared error), `rmse()` (root-mean-squared error), `medAbsErr()` (median absolute error), and `BayesRule()` are cross-validation criteria ("cost functions"), suitable for use with `cv()`.
 
--   `cvSelect()` cross-validates a selection procedure for a regression model. `cvSelect()` also supports parallel computations.
+-   `cv()` also can cross-validate a selection procedure (such as the following) for a regression model:
 
--   `selectStepAIC()` is a model-selection procedure, suitable for use with `cvSelect()`, based on the `stepAIC()` function in the **MASS** package.
+    - `cvModelList()` employs CV to select a model from among a number of candidates, and then cross-validates this model-selection procedure.
 
--   `selectTrans()` is a procedure for selecting predictor and response transformations in regression, suitable for use with `cvSelect()`, based on the `powerTransform()` function in the **car** package.
+    -   `selectStepAIC()` is a predictor-selection procedure based on the `stepAIC()` function in the **MASS** package.
 
--   `selectTransStepAIC()` is a procedure also suitable for use with `cvSelect()`,  that first selects predictor and response transformations and then selects predictors.
+    -   `selectTrans()` is a procedure for selecting predictor and response transformations in regression, based on the `powerTransform()` function in the **car** package.
 
-For additional information on using the **cv** package, see the "[Cross-validation of regression models](https://gmonette.github.io/cv/articles/cv.html)" vignette (`vignette("cv", package="cv")`). The **cv** package is designed to be extensible to other classes of regression models, other CV criteria, and other model-selection procedures; for details, see the "[Extending the cv package](https://gmonette.github.io/cv/articles/cv-extend.html)" vignette (`vignette("cv-extend", package="cv")`).
+    -   `selectTransStepAIC()` is a procedure that first selects predictor and response transformations and then selects predictors.
+
+For additional introductory information on using the **cv** package, see the "[Cross-validating regression models](https://gmonette.github.io/cv/articles/cv.html)" vignette (`vignette("cv", package="cv")`). There are also vignettes on [cross-validating mixed-effects models](https://gmonette.github.io/cv/articles/cv-mixed.html) (`vignette("cv-mixed", package="cv")`), [cross-validating model selection](https://gmonette.github.io/cv/articles/cv-selection.html) (`vignette("cv-selection", package="cv")`), and [computational and technical notes](https://gmonette.github.io/cv/articles/cv-notes.html) (`vignette("cv-notes", package="cv")`). The **cv** package is designed to be extensible to other classes of regression models, other CV criteria, and other model-selection procedures; for details, see the "[Extending the cv package](https://gmonette.github.io/cv/articles/cv-extend.html)" vignette (`vignette("cv-extend", package="cv")`).
 
 ## Installing the cv package
 
--- tests/testthat/test-cvSelect.R
+++ tests/testthat/test-cvSelect.R
@@ -1,7 +1,7 @@
 ## The tests in this file run only if the environment variable
 ##   RUN_ALL_CV_TESTS is set to true, in which case the tests create
-##   three data sets in the global environment, Auto, Cara, and
-##   Caravan.
+##   three data sets in the global environment, Auto, Cara,
+##   Caravan, and Prestige.
 
 if (Sys.getenv("RUN_ALL_CV_TESTS") == "true"){
 
@@ -13,10 +13,10 @@ data(Auto, package="ISLR2")
 m <- lm(mpg ~ . - name - origin, data=Auto)
 
 test_that("cvSelect naive vs Woodbury lm", {
-  expect_equal(cvSelect(selectStepAIC, Auto, k=5, seed=123,
-                        model=m, method="naive"),
-               cvSelect(selectStepAIC, Auto, k=5, seed=123,
-                        model=m, method="Woodbury")
+  expect_equal(cv(selectStepAIC, Auto, k=5, seed=123,
+                        working.model=m, method="naive"),
+               cv(selectStepAIC, Auto, k=5, seed=123,
+                        working.model=m, method="Woodbury")
   )
 })
 
@@ -24,10 +24,10 @@ test_that("cvSelect naive vs Woodbury lm", {
 #  for a lm
 
 test_that("cvSelect parallel lm", {
-  expect_equal(cvSelect(selectStepAIC, Auto, k=5, seed=123,
-                        model=m),
-               cvSelect(selectStepAIC, Auto, k=5, seed=123,
-                        model=m, ncores=2)
+  expect_equal(cv(selectStepAIC, Auto, k=5, seed=123,
+                        working.model=m),
+               cv(selectStepAIC, Auto, k=5, seed=123,
+                        working.model=m, ncores=2)
                )
 })
 
@@ -37,10 +37,10 @@ test_that("cvSelect parallel lm", {
 m.glm <- lm(mpg ~ . - name - origin, data=Auto)
 
 test_that("cvSelect exact vs Woodbury glm", {
-  expect_equal(cvSelect(selectStepAIC, Auto, k=5, seed=123,
-                        model=m.glm, method="exact"),
-               cvSelect(selectStepAIC, Auto, k=5, seed=123,
-                        model=m.glm, method="Woodbury")
+  expect_equal(cv(selectStepAIC, Auto, k=5, seed=123,
+                        working.model=m.glm, method="exact"),
+               cv(selectStepAIC, Auto, k=5, seed=123,
+                        working.model=m.glm, method="Woodbury")
   )
 })
 
@@ -48,18 +48,18 @@ test_that("cvSelect exact vs Woodbury glm", {
 # longer tests
 
 test_that("cvSelect exact vs hatvalues loo glm", {
-  expect_equal(cvSelect(selectStepAIC, Auto, k="loo",
-                        model=m.glm, method="exact"),
-               cvSelect(selectStepAIC, Auto, k="loo",
-                        model=m.glm, method="hatvalues")
+  expect_equal(cv(selectStepAIC, Auto, k="loo",
+                        working.model=m.glm, method="exact"),
+               cv(selectStepAIC, Auto, k="loo",
+                        working.model=m.glm, method="hatvalues")
   )
 })
 
 test_that("cvSelect Woodbury vs hatvalues loo glm", {
-  expect_equal(cvSelect(selectStepAIC, Auto, k="loo",
-                        model=m.glm, method="Woodbury"),
-               cvSelect(selectStepAIC, Auto, k="loo",
-                        model=m.glm, method="hatvalues")
+  expect_equal(cv(selectStepAIC, Auto, k="loo",
+                        working.model=m.glm, method="Woodbury"),
+               cv(selectStepAIC, Auto, k="loo",
+                        working.model=m.glm, method="hatvalues")
   )
 })
 
@@ -71,10 +71,118 @@ assign("Cara", Caravan[1:500, c(1:10, 86)], envir=.GlobalEnv)
 m.caravan <- glm(Purchase ~ ., data=Cara, family=binomial)
 
 test_that("cvSelect parallel glm", {
-  expect_equal(cvSelect(selectStepAIC, Cara, k=5, seed=123,
-                        model=m.caravan, criterion=BayesRule),
-               cvSelect(selectStepAIC, Cara, k=5, seed=123,
-                        model=m.caravan, ncores=2, criterion=BayesRule)
+  expect_equal(cv(selectStepAIC, Cara, k=5, seed=123,
+                        working.model=m.caravan, criterion=BayesRule),
+               cv(selectStepAIC, Cara, k=5, seed=123,
+                        working.model=m.caravan, ncores=2, criterion=BayesRule)
+  )
+})
+
+data("Prestige", package="carData")
+m.pres <- lm(prestige ~ income + education + women, data=Prestige)
+
+test_that("cvSelect parallel selectTrans", {
+  expect_equal(
+    cv(selectTrans, data=Prestige,
+       working.model=m.pres, seed=1463,
+       predictors=c("income", "education", "women"),
+       response="prestige", family="yjPower"),
+    cv(selectTrans, data=Prestige,
+       working.model=m.pres, seed=1463,
+       predictors=c("income", "education", "women"),
+       response="prestige", family="yjPower",
+       ncores=2)
+  )
+})
+
+data("Auto", package="ISLR2")
+Auto$cylinders <- factor(Auto$cylinders,
+                         labels=c("3.4", "3.4", "5.6", "5.6", "8"))
+Auto$year <- as.factor(Auto$year)
+Auto$origin <- factor(Auto$origin,
+                      labels=c("America", "Europe", "Japan"))
+rownames(Auto) <- make.names(Auto$name, unique=TRUE)
+Auto$name <- NULL
+m.auto <- lm(mpg ~ ., data = Auto)
+num.predictors <- c("displacement", "horsepower", "weight", "acceleration")
+
+test_that("cvSelect parallel selectTransStepAIC", {
+  expect_equal(
+    cv(selectTransStepAIC, data=Auto, seed=76692,
+       working.model=m.auto, predictors=num.predictors,
+       response="mpg", AIC=FALSE, criterion=medAbsErr),
+    cv(selectTransStepAIC, data=Auto, seed=76692,
+       working.model=m.auto, predictors=num.predictors,
+       response="mpg", AIC=FALSE, criterion=medAbsErr,
+       ncores=2)
+  )
+})
+
+# test recursive CV
+
+data("Auto", package="ISLR2")
+for (p in 1:10){
+  command <- paste0("m.", p, "<- lm(mpg ~ poly(horsepower, ", p,
+                    "), data=Auto)")
+  eval(parse(text=command))
+}
+
+cv1 <- cv(selectModelList, Auto, k=10, k.recurse="loo",
+          working.model=models(m.1, m.2, m.3, m.4, m.5,
+                               m.6, m.7, m.8, m.9, m.10),
+          save.model=TRUE,
+          seed=123)
+cv2 <- cv(models(m.1, m.2, m.3, m.4, m.5,
+                 m.6, m.7, m.8, m.9, m.10),
+          recursive=TRUE,
+          k=10, k.recurse="loo",
+          save.model=TRUE,
+          seed=123)
+cv1$criterion <- cv2$criterion <- NULL
+test_that("cv(SelectModelList()) and cv.modList() produce same recursive CV/k-LOO", {
+  expect_equal(cv1, cv2)
+})
+
+cv1 <- cv(selectModelList, Auto,
+          working.model=models(m.1, m.2, m.3, m.4, m.5,
+                               m.6, m.7, m.8, m.9, m.10),
+          save.model=TRUE,
+          seed=123)
+cv2 <- cv(models(m.1, m.2, m.3, m.4, m.5,
+                 m.6, m.7, m.8, m.9, m.10),
+          recursive=TRUE,
+          save.model=TRUE,
+          seed=123)
+cv1$criterion <- cv2$criterion <- NULL
+test_that("cv(SelectModelList()) and cv.modList() produce same recursive CV/k-fold", {
+  expect_equal(cv1, cv2)
+})
+
+test_that("recursive SelectModelList() works with parallel computation loo", {
+  expect_equal(
+    cv(selectModelList, Auto, k="loo",
+       working.model=models(m.1, m.2, m.3, m.4, m.5,
+                            m.6, m.7, m.8, m.9, m.10),
+       save.model=TRUE),
+    cv(selectModelList, Auto, k="loo",
+       working.model=models(m.1, m.2, m.3, m.4, m.5,
+                            m.6, m.7, m.8, m.9, m.10),
+       save.model=TRUE,
+       ncores=2)
+  )
+})
+
+test_that("recursive SelectModelList() works with parallel computation k-fold", {
+  expect_equal(
+    cv(selectModelList, Auto,
+       working.model=models(m.1, m.2, m.3, m.4, m.5,
+                            m.6, m.7, m.8, m.9, m.10),
+       save.model=TRUE),
+    cv(selectModelList, Auto,
+       working.model=models(m.1, m.2, m.3, m.4, m.5,
+                            m.6, m.7, m.8, m.9, m.10),
+       save.model=TRUE,
+       ncores=2)
   )
 })
 
